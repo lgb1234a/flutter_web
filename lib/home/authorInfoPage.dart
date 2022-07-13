4148726b037aa854dbf08df0668f215387b3f0a2
chenyn <1048196441@qq.com>
2022-07-13 11:38:36 +0800

    fix:处理个人信息展示

--- lib/home/authorInfoPage.dart
+++ lib/home/authorInfoPage.dart
@@ -4,6 +4,24 @@ import 'bloc/homeCubit.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AuthorInfoPage extends StatelessWidget {
+  void _clickedCategoryBtn() {}
+  void _clickedGithubBtn() {}
+  void _clickedLinkedInBtn() {}
+  Widget _createBtn(String iconPath, String tip, VoidCallback callback) {
+    return FloatingActionButton.small(
+      onPressed: callback,
+      tooltip: tip,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.w),
+        child: Image.asset(
+          iconPath,
+          width: 60.w,
+          height: 60.w,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<HomeCubit, bool>(
@@ -12,15 +30,28 @@ class AuthorInfoPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/images/avatar.png',
-                width: 120.w,
-                height: 120.w,
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.w),
+                child: Image.asset(
+                  'assets/images/avatar.png',
+                  width: 120.w,
+                  height: 120.w,
+                ),
               ),
-              FloatingActionButton(
-                onPressed: () => print('---------'),
-                child: const Icon(Icons.add),
+              Container(
+                height: 15,
               ),
+              _createBtn('assets/images/blog.gif', '归档', _clickedCategoryBtn),
+              Container(
+                height: 15,
+              ),
+              _createBtn(
+                  'assets/images/github.gif', 'Github', _clickedGithubBtn),
+              Container(
+                height: 15,
+              ),
+              _createBtn(
+                  'assets/images/linkedin.gif', '简历', _clickedLinkedInBtn),
             ]);
       },
     );
